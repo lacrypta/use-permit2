b09ac19d8f59f7672a999bd4278f760567239c45
Agustin Kassis <webmaster@masize.com>
2023-02-11 21:39:48 -0300

    added ConnectKit and WagmiConfig providers

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { WagmiConfig, createClient } from "wagmi";
+import { polygon } from "wagmi/chains";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Your App Name",
+    chains: [polygon],
+  })
+);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <App />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
